971621ade174199c2ea4369c66fec301bc5ed9e6
SIMusleh <128906425+SIMusleh@users.noreply.github.com>
2025-11-03 00:43:53 +0000

    added notebook.h file

---
+++ notebook.h
@@ -0,0 +1,47 @@
+/* Program name: Notebook.h
+* Author: Sahar Musleh
+* Date last updated: 10/31/25
+* Purpose: defines the Notebook class and the methods it uses to store notebook information.
+*/
+
+#ifndef NOTEBOOK_H
+#define NOTEBOOK_H
+
+#include <string>
+
+class Notebook {
+    private:
+        double width;
+        double height;
+        int numPages;
+        std::string paperType;
+        std::string format;
+        std::string paperColor;
+        std::string coverType;
+        std::string coverDesign;
+
+    public:
+        void setWidth(double w);
+        void setHeight(double h);
+        void setNumPages(int n);
+        void setPaperType(std::string pt);
+        void setFormat(std::string f);
+        void setPaperColor(std::string pc);
+        void setCoverType(std::string ct);
+        void setCoverDesign(std::string cd);
+
+            double getWidth() const;
+            double getHeight() const;
+            int getNumPages() const;
+            std::string getPaperType() const;
+            std::string getFormat() const;
+            std::string getPaperColor() const;
+            std::string getCoverDesign() const;
+            std::string getCoverDesign() const;
+
+            std::string toString() const;
+
+
+};
+
+#endif
