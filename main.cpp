0835218dd5b6407a8c2e6013e7d032fe6a36e76b
SIMusleh <128906425+SIMusleh@users.noreply.github.com>
2025-11-03 00:50:22 +0000

    added main.cpp file

---
+++ main.cpp
@@ -0,0 +1,52 @@
+/* Program name: main.cpp
+ * Author: Sahar Musleh
+ * Date last updated: 11/02/2025
+ * Purpose: Gets notebook details from the user and displays the final notebook.
+ */
+
+ #include <iostream>
+ #include "notebook.h"
+
+int main() {
+    notebook mynotebook;
+    std::string inputstr;
+    int inputInt;
+    double inputDouble;
+
+    std::cout << "Enter the name of the cover color or design: ";
+    std::getline(std::cin,inputstr);
+    mynotebook.setCoverDesign(inputstr);
+
+    std::cout << "Enter the cover type: ";
+    std::getline(std::cin,inputstr);
+    mynotebook.setCoverType(inputstr);
+
+    std::cout << "Enter the paper color: ";
+    std::getline(std::cin,inputstr);
+    mynotebook.setPaperColor(inputstr);
+
+    std::cout << "Enter the notebook format: ";
+    std::getline(std::cin,inputstr);
+    mynotebook.setFormat(inputstr);
+
+    std::cout << "Enter the type of paper: ";
+    std::getline(std::cin,inputstr);
+    mynotebook.setPaperType(inputstr);
+
+    std::cout << "Enter the number of pages: ";
+    std::cin >> inputInt;
+    mynotebook.setNumPages(inputInt);
+
+    std::cout << "Enter the notebook height: ";
+    std::cin >> inputDouble;
+    mynotebook.setHeight(inputDouble);
+
+    std::cout << "Enter the notebook width: ";
+    std::cin >> inputDouble;
+    mynotebook.setWidth(inputDouble);
+
+    std::cout << "\nHere is your notebook: \n";
+    std::cout << mynotebook.toString() << std::endl;
+
+    return 0;
+}
